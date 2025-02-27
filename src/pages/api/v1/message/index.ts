cdf5abdbeb3eac1e2e65b7c8c5411a09e278d8b5
Tsotne Gvadzabia <32150283+tsotimus@users.noreply.github.com>
2025-02-27 14:45:30 +0000

    remove mocks

--- src/pages/api/v1/message/index.ts
+++ src/pages/api/v1/message/index.ts
@@ -1,32 +1,8 @@
 import dbConnect from "@/lib/dbConnect"
 import Message from "@/models/Message"
-import { MessageDisplay, MessageRequestSchema } from "@/types/message"
+import { MessageRequestSchema } from "@/types/message"
 import { NextApiRequest, NextApiResponse } from "next"
 
-
-const MOCKS: MessageDisplay[] = [
-  {
-    id: "123",
-    createdBy: "user_2tcybdGFHbut2LkdDVxGUvVAJg6",
-    text: "Hello everyone!",
-    updatedAt: new Date().toISOString(),
-    createdAt: new Date().toISOString(),
-  },
-  {
-    id: "125",
-    createdBy: "user_2tcybdGFHbut2LkdDVxGUvVAJg6",
-    text: "Hello",
-    updatedAt: new Date().toISOString(),
-    createdAt: new Date().toISOString(),
-  },
-  {
-    id: "124",
-    createdBy: "user_2tcybdGFHbut2LkdDVxGUvVAJg6",
-    text: "Hello to you too!",
-    updatedAt: new Date().toISOString(),
-    createdAt: new Date().toISOString(),
-  }
-]
 
 const handler = async(req: NextApiRequest, res: NextApiResponse) => {
 
