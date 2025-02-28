b32135190cfb3e4ef74ea2e59a280eb1ddd9f05a
Tsotne Gvadzabia <32150283+tsotimus@users.noreply.github.com>
2025-02-28 11:10:42 +0000

    update nav

--- src/features/home/Nav.tsx
+++ src/features/home/Nav.tsx
@@ -1,27 +1,29 @@
 "use client"
+import { Button } from "@/components/ui/button"
 import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
-import { SignOutButton, useUser } from "@clerk/nextjs"
+import { SignOutButton, useClerk, useUser,  } from "@clerk/nextjs"
 import Link from "next/link"
 
 const NAV = () => {
-    const {isSignedIn} = useUser()
+    const { isSignedIn } = useUser()
+    const { signOut } = useClerk()
     return (
         <div className="flex justify-between items-center w-full px-40 py-4">
             {
                 isSignedIn ? (
                     <NavigationMenu className="flex justify-between items-center w-full">
                         <NavigationMenuList className="flex justify-between items-center w-full">
-                        <NavigationMenuItem>
-                        <Link href="/" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                            All Rooms
-                            </NavigationMenuLink>
-                        </Link>
-                        </NavigationMenuItem>
                             <NavigationMenuItem>
-                                <SignOutButton/>
+                            <Link href="/" legacyBehavior passHref>
+                                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                All Rooms
+                                </NavigationMenuLink>
+                            </Link>
                             </NavigationMenuItem>
-                        </NavigationMenuList>   
+                            <NavigationMenuItem>
+                                <Button variant="outline" className="cursor-pointer" onClick={() => signOut()}>Sign Out</Button>
+                            </NavigationMenuItem>
+                        </NavigationMenuList>
                     </NavigationMenu>
                 ): null
             }
